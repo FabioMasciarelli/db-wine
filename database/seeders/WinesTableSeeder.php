1f3a228d1b9eb32e0543bcc3fcac797324c6dbd2
francesco-mauro <francesco.mauro180@gmail.com>
2024-06-13 17:23:20 +0200

    feat: last push

--- database/seeders/WinesTableSeeder.php
+++ database/seeders/WinesTableSeeder.php
@@ -18,9 +18,10 @@ class WinesTableSeeder extends Seeder
         $response = Http::withOptions(['verify' => false])->get('https://api.sampleapis.com/wines/reds');
         $data = $response->json();
         dd($data);
-        foreach ($data['items'] as $Wine) {
+        foreach ($data['items'] as $wine) {
             $newWine = new Wine();
-            $newWine -> 
+            $newWine->brand=$wine['winery']; 
+            $newWine->save(); 
         }
             
     }
